26a286a59572a53b68dd04aed5fd30f92b61552e
storiaca <storiaca@gmail.com>
2020-04-16 11:06:27 +0200

    Structure Crads component

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,12 +1,53 @@
 import React from "react";
 
+import { Card, CardContent, Typography, Grid } from "@material-ui/core";
+
+import styles from "./Cards.module.css";
+
 const Cards = (props) => {
   const { data } = props;
   console.log(data);
 
   return (
-    <div>
-      <h2>Cards</h2>
+    <div className={styles.container}>
+      <Grid container spacing={3} justify="center">
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Infected
+            </Typography>
+            <Typography variant="h5">REAL DATA</Typography>
+            <Typography color="textSecondary">REAL DATE</Typography>
+            <Typography variant="body2">
+              Number of active casses of COVID-19
+            </Typography>
+          </CardContent>
+        </Grid>
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Recovered
+            </Typography>
+            <Typography variant="h5">REAL DATA</Typography>
+            <Typography color="textSecondary">REAL DATE</Typography>
+            <Typography variant="body2">
+              Number of recoveries from COVID-19
+            </Typography>
+          </CardContent>
+        </Grid>
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Deaths
+            </Typography>
+            <Typography variant="h5">REAL DATA</Typography>
+            <Typography color="textSecondary">REAL DATE</Typography>
+            <Typography variant="body2">
+              Number of deaths casued by COVID-19
+            </Typography>
+          </CardContent>
+        </Grid>
+      </Grid>
     </div>
   );
 };
